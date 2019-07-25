0d444785ac78b43ec45407b457673cdad223e14a
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2019-07-25 20:10:50 +0200

    Limiting player position

--- app.js
+++ app.js
@@ -90,6 +90,10 @@ const play = async () => {
   animate();
 
   window.addEventListener("keyup", event => {
+    const currentPosition = {
+      x: player.x,
+      y: player.y
+    };
     if (event.code === "ArrowRight") {
       player.x = player.x + 1;
     }
@@ -101,6 +105,12 @@ const play = async () => {
     }
     if (event.code === "ArrowDown") {
       player.y = player.y + 1;
+    }
+    if (player.x < 0 || player.x > 2) {
+      player.x = currentPosition.x;
+    }
+    if (player.y < 0 || player.y > 2) {
+      player.y = currentPosition.y;
     }
   });
 };
